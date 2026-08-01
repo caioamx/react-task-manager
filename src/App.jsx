78f2c4a03a392aec490d511a59eee18f61ccf69d
caioMaga <magalhaescaioandre@gmail.com>
2026-08-01 15:06:29 -0300

    Initial Commit
    Commit Inicial da configuração do Projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className=" flex h-screen items-center justify-center bg-[#1C1B1B]">
+      <h1 className="text-2xl font-bold text-white"> Olá Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
